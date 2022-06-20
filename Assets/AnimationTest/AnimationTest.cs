90114909349d15ef54f9f019204db68a38963b46
임재경 <jakeim11@zempot.com>
2022-06-20 09:54:24 +0900

    Any State에서 Integer 변수 사용 예

--- Assets/AnimationTest/AnimationTest.cs
+++ Assets/AnimationTest/AnimationTest.cs
@@ -6,18 +6,30 @@ public class AnimationTest : MonoBehaviour
 {
     public Animator animator;
 
+    private void Start()
+    {
+        animator.SetInteger("intTrigger", -1);
+    }
+
     public void OnClickAnim1Button()
     {
-        animator.ResetTrigger("anim1");
-        animator.SetTrigger("anim1");
+        // animator.ResetTrigger("anim1");
+        // animator.SetTrigger("anim1");
+        animator.SetInteger("intTrigger", 1);
+        animator.SetInteger("intTrigger", -1);
+
     }
 
     public void OnClickAnim2Button()
     {
-        animator.SetTrigger("anim2");
+        // animator.SetTrigger("anim2");
+        animator.SetInteger("intTrigger", 2);
+        animator.SetInteger("intTrigger", -1);
     }
     public void OnClickIdleButton()
     {
-        animator.SetTrigger("Idle");
+        // animator.SetTrigger("Idle");
+        animator.SetInteger("intTrigger", 0);
+        animator.SetInteger("intTrigger", -1);
     }
 }
