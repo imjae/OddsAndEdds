b71de045af20e6e2ad7f9b51c2a51e2105bf3678
임재경 <jakeim11@zempot.com>
2022-07-23 10:22:49 +0900

    스크립트 기본 구조

--- Assets/CoroutineTest/Script/CoroutineTestScript.cs
+++ Assets/CoroutineTest/Script/CoroutineTestScript.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoroutineTestScript : MonoBehaviour
+namespace CoroutineTest
 {
-    // Start is called before the first frame update
-    void Start()
+    public class CoroutineTestScript : MonoBehaviour
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnClickCoroutineStart()
+        {
+
+        }
+        public void OnClickCoroutineStop()
+        {
+
+        }
     }
 }
