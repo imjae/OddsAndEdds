7d76be8c487cb13d238d38ce1ffd496937981e44
임재경 <jakeim11@zempot.com>
2022-11-29 14:23:56 +0900

    Add이벤트 테스트 스크립트 추가

--- Assets/EventTest/Script/AddEventTestScript.cs
+++ Assets/EventTest/Script/AddEventTestScript.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AddEventTestScript : MonoBehaviour
 {
+    public Button button;
+
+    private void Awake()
+    {
+        button.onClick.AddListener(ListenerTest);
+    }
+
+    public void ListenerTest()
+    {
+        Debug.Log($"짜잔~");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
