1cad9aa6eb9780ddffb1a36b76da9bf6c18a800e
임재경 <jakeim11@zempot.com>
2022-12-07 16:55:28 +0900

    테스트 완료

--- Assets/AnimatorLayerTest/Script/CubeTestScript.cs
+++ Assets/AnimatorLayerTest/Script/CubeTestScript.cs
@@ -4,5 +4,26 @@ using UnityEngine;
 
 public class CubeTestScript : MonoBehaviour
 {
-    
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void OnPositionTrigger()
+    {
+        animator.SetTrigger("Position");
+    }
+
+    public void OnSizeTrigger()
+    {
+        animator.SetTrigger("Size");
+    }
+
+    public void OnComplexTrigger()
+    {
+        animator.SetTrigger("Position");
+        animator.SetTrigger("Size");
+    }
 }
